7fc0bfc8db4d6f9e2ad49ac82e08a7a17f089f7c
27031973gau-73 <gau-73@mail.ru>
2024-05-24 12:35:08 +0500

    Add files via upload

---
+++ Homework1.ru/main.py
@@ -0,0 +1,10 @@
+home_work = 12
+print(home_work)
+time_in_money = 1.5
+print(time_in_money)
+a_new_lesson = 'Python'
+print(a_new_lesson)
+time_limit = 1.5/12
+print(time_limit)
+print('Курс', ':', a_new_lesson, ',', 'всего', 'задач', ':', home_work, ',', 'затрачено', 'часов', ':', time_in_money,
+      ',', 'среднее', 'время', 'выполнения', time_limit, 'часа', '.')
